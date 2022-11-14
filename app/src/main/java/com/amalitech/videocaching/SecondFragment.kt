49b2620843bdef36408f494d0ee807f06dc130eb
ben rapha <bensek73@gmail.com>
2022-11-14 10:11:53 +0200

    added video caching

---
+++ app/src/main/java/com/amalitech/videocaching/SecondFragment.kt
@@ -0,0 +1,85 @@
+package com.amalitech.videocaching
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.amalitech.videocaching.databinding.FragmentSecondBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.upstream.HttpDataSource
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+private var _binding: FragmentSecondBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private lateinit var mHttpDataSourceFactory: HttpDataSource.Factory
+    private lateinit var mDefaultDataSourceFactory: DefaultDataSourceFactory
+    private lateinit var mCacheDataSourceFactory: DataSource.Factory
+    private lateinit var exoPlayer: ExoPlayer
+    private val cache: SimpleCache = VideoApp.cache
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+      _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+        mHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        this.mDefaultDataSourceFactory = DefaultDataSourceFactory(
+            requireContext(), mHttpDataSourceFactory)
+
+        mCacheDataSourceFactory = CacheDataSource.Factory()
+            .setCache(cache)
+            .setUpstreamDataSourceFactory(mHttpDataSourceFactory)
+            .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
+
+        exoPlayer = ExoPlayer.Builder(requireContext())
+            .setMediaSourceFactory(DefaultMediaSourceFactory(mCacheDataSourceFactory)).build()
+
+        exoPlayer = ExoPlayer.Builder(requireContext()).build()
+
+        val videoUrl = "https://cdn.coverr.co/videos/coverr-skating-with-a-dog-1353/1080p.mp4"
+        val videoUri = Uri.parse(videoUrl)
+        val mediaItem = MediaItem.fromUri(videoUri)
+        val mediaSource =
+            ProgressiveMediaSource.Factory(mCacheDataSourceFactory).createMediaSource(mediaItem)
+
+        binding.playerView.player = exoPlayer
+        exoPlayer.playWhenReady = true
+        exoPlayer.seekTo(0, 0)
+        //exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.setMediaSource(mediaSource, true)
+        exoPlayer.prepare()
+
+      return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
